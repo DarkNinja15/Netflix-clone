9ffe940f45877e05002d351cc272264404c272e6
Animesh Shukla <animeshshukla1518@gmail.com>
2022-09-06 13:41:46 +0530

    search screen completed

--- netflix_clone/lib/screens/search.dart
+++ netflix_clone/lib/screens/search.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
@@ -6,8 +8,121 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Search'),
+        backgroundColor: Colors.black,
+        leading: const Icon(
+          Icons.search,
+          color: Colors.white,
+        ),
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search for a movie,show,genre',
+            hintStyle: const TextStyle(
+              color: Colors.white,
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.mic,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 8.0),
+              child: Text(
+                'Popular Searches',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 570,
+              child: FutureBuilder<QuerySnapshot>(
+                future: FirebaseFirestore.instance.collection('popular').get(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Lottie.asset('assets/animation.json');
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: GestureDetector(
+                            onTap: null,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade500,
+                                gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Colors.grey.shade500,
+                                      Colors.white,
+                                      Colors.blueGrey,
+                                    ]),
+                              ),
+                              child: SizedBox(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    SizedBox(
+                                      height: 90,
+                                      width: 150,
+                                      child: Image.network(
+                                        snapshot.data!.docs[index]['image'],
+                                        fit: BoxFit.fitWidth,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Container(
+                                        width: 100,
+                                        child: Text(
+                                          snapshot.data!.docs[index]['name'],
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.play_arrow_rounded,
+                                        color: Colors.black,
+                                        size: 30,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
